13a723432a3bdaff458bd0338478ca59f751a597
cyraimb <cyra.imbuido@gmail.com>
2023-12-09 01:02:23 +0800

    Added forms for updates

---
+++ Controllers/UpdatePetAdoption.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurrfectPair.Controllers
+{
+    public class UpdatePetAdoption : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UpdateSubmitPet.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurrfectPair.Controllers
+{
+    public class UpdateSubmitPet : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
